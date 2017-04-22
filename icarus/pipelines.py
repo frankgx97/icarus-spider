bddb6c9f0c6f6f5b11d2f0e6228048ddcc8cd004
frank <i@nyan.im>
2017-04-22 14:09:24 +0800

    init

---
+++ icarus/pipelines.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import sys
+import os
+from db import *
+ 
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+url_prefix = 'http://static.guoduhao.cn/nyanfm/bg/'
+download_path = '/var/web/static/nyanfm/bg/'
+
+class IcarusPipeline(object):
+    def process_item(self, item, spider):
+        print '============='
+        print item['image_url'][0]
+        bg = Bg(
+            url=url_prefix + item['image_url'][0].split('/')[7]
+        )
+        try:
+            bg.save()
+            print '##db wrote##'
+        except:
+            print '!!!db failed!!!'
+        os.system("wget '"+item['image_url'][0]+"' -P '"+download_path+"'")
+        print '*************'
---
+++ icarus/spiders/spider.py
@@ -0,0 +1,46 @@
+#coding:utf8
+import sys
+import scrapy
+from scrapy.selector import Selector
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from icarus.items import IcarusItem
+ 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+ 
+ 
+class icarus_spider(CrawlSpider):#CrawlSpider类继承自Spider类，可以通过定义rules属性实现递归抓取
+    name = "icarus"
+    allowed_domains = ["icarus.silversky.moe"]#允许抓取的域名
+    start_urls = ["http://icarus.silversky.moe:666/category/illustration/super-res-wallpaper/page/1"]#起始url
+ 
+    #定义rules属性
+    rules = (
+        Rule(SgmlLinkExtractor(allow=(r'\/category\/illustration\/super-res-wallpaper\/page\/\d')),#使用正则表达式匹配符合要求的网页
+             callback=None,#回调函数
+             follow=True#是否跟进此页上的url，如果callback为空的话则默认为true
+            ),
+        Rule(SgmlLinkExtractor(allow=(r'\/illustration\/\d+')),
+             callback='download_image',
+             follow=False
+            ),
+    )
+
+    def download_image(self,response):
+        sel = Selector(response)
+        item = IcarusItem()
+        image_url = sel.xpath("//a[@class='highslide-image']/@href").extract()
+        item['image_url'] = [n for n in image_url]
+        yield item   
+ 
+    def parse_item(self, response):
+    #抓取网页后会默认调用parse函数进行处理
+        sel = Selector(response)
+        #使用xpath选取html元素
+        title = sel.xpath("//title").extract()
+        item = IcarusItem()
+        #由于sel.xpath().extract()返回的是一个list，所以先使用for遍历
+        item['title'] = [n for n in title]#将title中的内容赋值给item
+ 
+        yield item#将item中的数据交由pipeline进一步处理
